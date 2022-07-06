8ef829625bcdb2d17ba0208c5940c06d6955b3d9
Juan Lozano <juancloz@gmail.com>
2022-07-06 14:36:13 -0600

    Minor updates. Still very much a work in progress.

--- coefficient_of_correlation.py
+++ coefficient_of_correlation.py
@@ -5,6 +5,6 @@ import pandas as pd
 x = [2,5,3.5,6.2,4.5,1.5,7,4.1,3,5.6]
 y = [17,6,10,5,8,18,6,8,13,9]
 
-df = pd.DataFrame([x],columns=['x'])
+df = pd.DataFrame([x,y],columns=['x','y'])
 
 print(df)

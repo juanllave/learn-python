f7100c511574ee0fbeddf07289a19f308e007d98
Juan Lozano <juancloz@gmail.com>
2022-07-06 14:35:26 -0600

    Some additions. Managed to install pip and pandas.

--- coefficient_of_correlation.py
+++ coefficient_of_correlation.py
@@ -1,11 +1,10 @@
 import io
+import pandas as pd
+
 
 x = [2,5,3.5,6.2,4.5,1.5,7,4.1,3,5.6]
 y = [17,6,10,5,8,18,6,8,13,9]
 
-df = x,y
+df = pd.DataFrame([x],columns=['x'])
 
-df['x2']=df[x**2]
-
-
-print(2^2)
+print(df)
